bbd7a860e7af8ae74b550e6cecade393a3cd0ca0
gureks <gurek15033@iiitd.ac.in>
2017-11-07 16:34:58 +0530

    added follow filter to the farmer

--- FriendsFarmer.py
+++ FriendsFarmer.py
@@ -28,6 +28,13 @@ def append_to_db(users):
 		db['users_to_follow'].update({'_id':i['_id']}, i, True)
 	print(db['users_to_follow'].count({}))
 
+def remove_from_db(users, collection):
+	# Remove from database
+	print("Initial " + collection + "size: " + str(db[collection].count({})))
+	for i in users:
+		db[collection].remove({'_id':i['_id']})
+	print("Updated " + collection + "size: " + str(db[collection].count({})))
+
 def collect_similar_interests():
 	# Get the latest tweets of the user
 	statuses = twitter.get_user_timeline(screen_name=user_to_collect,
@@ -121,6 +128,69 @@ def collect_followers_of_followers():
 						})
 	append_to_db(users)
 
+def follow_filter():
+	users_to_follow = db['users_to_follow'].find({}, no_cursor_timeout=True)
+	followers = db['followers'].find({}, no_cursor_timeout=True)
+
+	remove = list()
+
+	month = {'Jan' : 1, 'Feb' : 2, 'Mar' : 3, 'Apr' : 4, 'May' : 5, 'Jun' : 6, 'Jul' : 7, 'Aug' : 8, 'Sep' : 9, 'Oct' : 10, 'Nov' : 11, 'Dec' : 12}
+	# To be changed
+	last_date = datetime(2017, 1, 1, 00, 00, 00)
+
+	followers_usernames = []
+	for follower in followers:
+		followers_usernames.append(follower['username'])
+
+	c = 0
+	for user in users_to_follow:
+		user_name = user['username']
+		print(user_name)
+
+		if user_name in followers_usernames:
+ 			continue
+		else:
+			result = twitter.show_user(user_id=user['_id'], include_entities=True)
+
+			if result['protected']:
+				print("Private User - " + user_name)
+				remove.append(user['_id'])
+				c += 1
+				continue
+
+			if not result['statuses_count']:
+				print (user_name + " added to unfollowing list cause of inactivity.")
+				remove.append(user['_id'])
+				c += 1
+				continue
+
+			if 'status' not in result.keys():
+				print (user_name + " added to unfollowing list cause of inactivity.")
+				remove.append(user['_id'])
+				c += 1
+				continue
+
+			latest_tweet = result['status']['created_at']
+			latest_tweet_dt = datetime.strptime(latest_tweet, "%a %b %d %X %z %Y")
+
+			num_friends = result['friends_count']
+			num_followers = result['followers_count']
+
+			# Check if the last tweet is done before threshold last date
+			if latest_tweet_dt < last_date.replace(tzinfo=pytz.UTC):
+				print (user_name + " added to unfollowing list since his last tweet was done at " + str(latest_tweet_dt.date()))
+				remove.append(user['_id'])
+
+			elif float(num_followers)/(num_friends+1) > threshold_for_inout_ratio:
+				print (user_name + " added to unfollowing list since his in/out ratio was " +str(float(num_followers)/(num_friends+1)))
+				remove.append(user['_id'])
+
+			c += 1
+			if c%900==0:
+				print ("Sleeping for 1000 seconds")
+				time.sleep(1000)
+	remove_from_db(remove, 'users_to_follow')
+
 def follow_users():
 	users = db['users_to_follow'].find({}, no_cursor_timeout=True)
 	count = 0
@@ -153,10 +223,16 @@ def follow_users():
 if __name__ == '__main__':
 	try:
 		collect_suggested_users()
+		print("Suggested users collected... Sleeping 1000...")
 		time.sleep(1000)
 		collect_similar_interests()
+		print("Similar interest users collected... Sleeping 1000...")
 		time.sleep(1000)
 		collect_followers_of_followers()
+		print("Followers of follwers collected... Sleeping 1000...")
+		time.sleep(1000)
+		follow_filter()
+		print("Follow filter applied... Sleeping 1000...")
 		time.sleep(1000)
 		follow_users()
 	except Exception as e:
